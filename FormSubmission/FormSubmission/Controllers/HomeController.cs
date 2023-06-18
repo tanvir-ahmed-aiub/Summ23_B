adbf84cb796d9fa3499c961e56d02eb0b150a4ec
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2023-06-18 11:29:13 +0600

    Entiity Framework

--- FormSubmission/FormSubmission/Controllers/HomeController.cs
+++ FormSubmission/FormSubmission/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using FormSubmission.Models;
+﻿using FormSubmission.Db;
+using FormSubmission.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,11 @@ namespace FormSubmission.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult Index() {
+            var db =new UserDBSum23_BEntities();
+            var users = db.Users.ToList();
+            return View(Convert(users));
+        }
         [HttpGet]
         public ActionResult Login() {
             
@@ -26,11 +32,52 @@ namespace FormSubmission.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult SignUp(User user) {
+        public ActionResult SignUp(UserDTO user) {
             if (ModelState.IsValid) {
-                return RedirectToAction("Login");   
+                var db = new UserDBSum23_BEntities();
+                db.Users.Add(Convert(user));
+                db.SaveChanges();
+                return RedirectToAction("Index");
+                //return RedirectToAction("Login");   
+
             }
             return View(user);   
         }
+
+        User Convert(UserDTO user) {
+            var u = new User()
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Username = user.Username,
+                Profession=user.Profession,
+                Gender = user.Gender,
+            };
+            return u;
+        }
+        UserDTO Convert(User user) {
+            var u = new UserDTO()
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Username = user.Username,
+                Profession = user.Profession,
+                Gender = user.Gender,
+            };
+            return u;
+        }
+        List<UserDTO> Convert(List<User> users) {
+            var urs = new List<UserDTO>();
+            foreach (var item in users) { 
+                urs.Add(Convert(item));
+            }
+            return urs;
+        }
+
+
+
+
+        
+
     }
 }
---
+++ FormSubmission/FormSubmission/Models/UserDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FormSubmission.Models
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Please Provide Name")]
+        [StringLength(10, ErrorMessage ="Name cant exceed 10 characters")]
+        public string Name { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string Profession { get; set; }
+        public string[] Hobbies { get; set; }
+    }
+}
